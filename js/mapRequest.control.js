d3cb29aa20afd385b746a99741608ab73c8fdd4d
Michal Zimmermann <zimmicz@gmail.com>
2018-12-20 09:43:34 +0100

    Add link to home page

--- js/mapRequest.control.js
+++ js/mapRequest.control.js
@@ -11,6 +11,7 @@ L.Control.MapRequest = L.Control.extend({
         let self = this;
         let container = L.DomUtil.create("div", "bm-map-request");
         container.innerHTML = "<p>Would you like to play some other map? <a target='blank' href='https://feedback.userreport.com/2229a84e-c2fa-4427-99ab-27639f103569/'>Issue a map request.</a></p>";
+        container.innerHTML += "<p>Chose the wrong map? <a href='/'>Choose again!</a></p>"
 
         L.DomEvent.disableClickPropagation(container);
         L.DomEvent.disableScrollPropagation(container);
